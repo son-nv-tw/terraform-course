8eba831fce70f45da1ba54292aa9314e71ba0a01
Son Nguyen <nvs2394@gmail.com>
2022-01-24 17:53:03 +0800

    Update backend.tf

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,13 +1,7 @@
-provider "aws" {
-  region  = var.region
-  profile = "myprofile"
-}
-
 terraform {
  backend "s3" {
    bucket = "terraform-state-dpadp4l2"
    key    = "terraform.tfstate"
    region = "eu-west-1"
-   profile = "myprofile"
  }
 }
